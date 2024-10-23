d4c91910eb51276f063992bb51a2d5c67cb4e5c2
Qingnan Zhou <qzhou@adobe.com>
2024-10-23 10:49:50 -0400

    GCC warning fix.

--- cmake/recipes/external/poissonrecon.cmake
+++ cmake/recipes/external/poissonrecon.cmake
@@ -23,5 +23,12 @@ CPMAddPackage(
 )
 
 add_library(poissonrecon::poissonrecon INTERFACE IMPORTED GLOBAL)
-target_include_directories(poissonrecon::poissonrecon INTERFACE "${poissonrecon_SOURCE_DIR}/Src")
+target_include_directories(poissonrecon::poissonrecon SYSTEM INTERFACE "${poissonrecon_SOURCE_DIR}/Src")
 target_compile_definitions(poissonrecon::poissonrecon INTERFACE "SANITIZED_PR")
+
+# Silence some compiler warnings
+if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+    target_compile_options(poissonrecon::poissonrecon INTERFACE
+        "-Wno-maybe-uninitialized"
+    )
+endif()
