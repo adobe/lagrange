1d86198e1b64919fe1417b6ec5cfe2b8309712dc
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2025-06-26 17:33:22 -0700

    Update PoissonRecon dependency.

--- cmake/recipes/external/poissonrecon.cmake
+++ cmake/recipes/external/poissonrecon.cmake
@@ -18,8 +18,8 @@ message(STATUS "Third-party (external): creating target 'poissonrecon::poissonre
 include(CPM)
 CPMAddPackage(
     NAME poissonrecon
-    GITHUB_REPOSITORY jdumas/PoissonRecon
-    GIT_TAG d4b5c1ea50c5966232cd3eb051c46cbe56bd15d9
+    GITHUB_REPOSITORY mkazhdan/PoissonRecon
+    GIT_TAG 23446519aa82d625372935b76be3dab3b17b572d
 )
 
 add_library(poissonrecon::poissonrecon INTERFACE IMPORTED GLOBAL)
