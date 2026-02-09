fdf530c754461649a80c6989e10a1e8368e298d3
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2026-02-09 11:50:49 -0800

    Save space on boost source download.

--- cmake/recipes/external/Boost.cmake
+++ cmake/recipes/external/Boost.cmake
@@ -92,9 +92,8 @@ endif()
 include(CPM)
 CPMAddPackage(
     NAME Boost
-    VERSION 1.84.0
-    GITHUB_REPOSITORY "boostorg/boost"
-    GIT_TAG "boost-1.84.0"
+    URL https://github.com/boostorg/boost/releases/download/boost-1.84.0/boost-1.84.0.tar.xz
+    URL_HASH SHA256=2e64e5d79a738d0fa6fb546c6e5c2bd28f88d268a2a080546f74e5ff98f29d0e
     EXCLUDE_FROM_ALL ON
     ${BOOST_PATCHES}
 )
