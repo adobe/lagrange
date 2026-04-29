1fa7968237907ca9fc83e5463a9188825b7a32f5
Qingnan Zhou <qzhou@adobe.com>
2026-04-28 21:55:24 -0400

    Add copyright header to .ts files.

--- modules/io/js/ts/io.ts
+++ modules/io/js/ts/io.ts
@@ -1,3 +1,15 @@
+/*
+ * Copyright 2026 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
 /**
  * Read and write mesh / scene files from in-memory byte buffers — no
  * filesystem access. To load from a URL, `fetch` it first and pass the
