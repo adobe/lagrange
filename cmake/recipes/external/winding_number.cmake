169de9793ae982bf53a5907156867ce916513e01
Jérémie Dumas <jdumas@users.noreply.github.com>
2026-06-25 11:21:52 -0700

    Remove MSVC ARM64 patch handling from CMake

--- cmake/recipes/external/winding_number.cmake
+++ cmake/recipes/external/winding_number.cmake
@@ -19,15 +19,6 @@ lagrange_find_package(TBB CONFIG REQUIRED)
 include(simde)
 
 include(CPM)
-set(WINDINGNUMBER_PATCHES "")
-if(MSVC AND CMAKE_SYSTEM_PROCESSOR STREQUAL "ARM64")
-    # On MSVC ARM64, simde__m128 and simde__m128i are both __n128, making plain typedefs
-    # identical and breaking all overloaded functions. Patch VM_SSEFunc.h to use distinct
-    # wrapper structs instead. Use git apply --ignore-whitespace for robust CRLF handling.
-    find_package(Git REQUIRED QUIET)
-    set(_wn_patch "${CMAKE_CURRENT_LIST_DIR}/winding-number-winarm.patch")
-    set(WINDINGNUMBER_PATCHES PATCH_COMMAND "${GIT_EXECUTABLE}" apply --ignore-whitespace "${_wn_patch}")
-endif()
 CPMAddPackage(
     NAME WindingNumber
     GITHUB_REPOSITORY jdumas/WindingNumber
